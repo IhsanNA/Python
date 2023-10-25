30a4ac6a89ef1da759bdf07f93f5c2c66f38db0b
IhsanNA <144515726+IhsanNA@users.noreply.github.com>
2023-10-25 22:27:14 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,11 +1,15 @@
-No1 = int(input('Masukkan nomor pertama : '))
-No2 = int(input('Masukkan nomor kedua : '))
+# input var angka ke 1 dan ke 2
+No1 = int(input('Masukkan angka pertama : '))
+No2 = int(input('Masukkan angka kedua : '))
+
+# var perhitungan 
 tambah = No1 + No2
 kurang = No1 - No2
 kali = No1 * No2
 bagi = No1 // No2
 hasil = 'Hasil dari '
 
+# print daftar metode perhitungan
 print('===================================')
 print('pilih metode perhitungan')
 print('- tambah')
@@ -14,8 +18,10 @@ print('- kali')
 print('- bagi')
 print('===================================')
 
+# input metode perhitungan
 opsi = str(input('Masukkan metode perhitungan : '))
 
+# pengkondisian
 if opsi == 'tambah':
     print(hasil + str(No1) + ' + ' + str(No2) + ' = ' + str(tambah))
 elif opsi == 'kurang':
